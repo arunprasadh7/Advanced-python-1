9fe3cc36f5eab35b0c915adc5061eab7a13db1e6
arunprasadh7 <m.arunprasadh96@gmail.com>
2023-10-17 04:02:15 +0530

    Functions with multiple args

--- 3 Functions-2.py
+++ 3 Functions-2.py
@@ -1,9 +1,27 @@
 # Functions 2
 
 def greet(name='Arc'):
-	print(f'Hi {name}. How you doing Future Millionaire!!')
+    print(f'Hi {name}. How you doing Future Millionaire!!')
 
 
 g1 = greet('Arun')
 g2 = greet('Prasadh')
 g3 = greet()
+
+
+# functions with multiple args
+
+def sum(x, y):
+    c = x + y
+    print(c)
+
+
+sum(5,5)
+sum(10,10)
+
+
+def sub(x,y):
+	print(x-y)
+
+sub(10,5)
+sub(5,10)
