093b8be4ca5f80dcbfefcbf15e209afa046582d4
arunprasadh7 <m.arunprasadh96@gmail.com>
2023-10-17 03:02:37 +0530

    Classes & Objects

---
+++ 1 Classes and objects.py
@@ -0,0 +1,19 @@
+# 1 Classes and objects
+# classes - blueprint of a object
+# object - instances of a class
+
+# attributes - variables
+# methods - funtions
+
+class Person:
+    print('Hello!')
+    a = 10
+    print(a)
+
+
+print(Person)
+
+
+p1 = Person()
+print(p1.a)
+print(type(p1))
